2f66a9f8482004e39163f477220e6a262af00d36
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-01 16:49:50 -0300

    add preload script

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * PRELOAD
+ * 
+ * loading will be end after document is loaded
+ */
+
+const preloader = document.querySelector("[data-preaload]");
+
+window.addEventListener("load", function () {
+  preloader.classList.add("loaded");
+  document.body.classList.add("loaded");
+});
